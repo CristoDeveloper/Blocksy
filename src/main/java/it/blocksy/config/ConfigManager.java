e3989381e51bbed0256fa49274cb405d126fca42
crxsto <azzolinichris@gmail.com>
2026-02-18 17:11:13 +0100

    Bugfixes

--- src/main/java/it/blocksy/config/ConfigManager.java
+++ src/main/java/it/blocksy/config/ConfigManager.java
@@ -3,6 +3,8 @@ package it.blocksy.config;
 import it.blocksy.Blocksy;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.io.File;
+
 public class ConfigManager {
     
     private final Blocksy plugin;
@@ -32,6 +34,17 @@ public class ConfigManager {
      * Ricarica la configurazione dal file config.yml
      */
     public void reloadConfig() {
+        File dataFolder = plugin.getDataFolder();
+        if (!dataFolder.exists() && !dataFolder.mkdirs()) {
+            plugin.getLogger().severe("Impossibile creare la cartella dati di Blocksy: " + dataFolder.getPath());
+            return;
+        }
+        
+        File configFile = new File(dataFolder, "config.yml");
+        if (!configFile.exists()) {
+            plugin.saveDefaultConfig();
+        }
+        
         plugin.reloadConfig();
         loadConfig();
     }
